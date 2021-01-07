b1947fb20fe6891fa9ce22f82316f87c21d7b4c9
DenisGromenkoV <156789qw@gmail.com>
2021-01-07 13:20:29 +0200

    first commit

---
+++ CoreAngJwt/ClientApp/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AuthModule, AuthService } from '@auth0/auth0-angular';
+import { AuthButtonComponent } from './auth-button/app.auth-button-component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    FetchDataComponent,
+    AuthButtonComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    AuthModule.forRoot({
+      domain: 'dev-aihiu-u1.eu.auth0.com',
+      clientId: 'HaLMXbjzzjpZ4jqfP5Oe950SJa5PRU9W'
+    }),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
